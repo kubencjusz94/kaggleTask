955baec971a416ecbcc6e625a16a78eeee8962a6
kubencjusz94 <k.fabisiak94@gmail.com>
2018-12-14 09:47:27 +0100

    kubencjusz94

---
+++ dataset/app.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import sqlalchemy
+
+db = False
+while not db:
+    try:
+        engine = sqlalchemy.create_engine('postgresql://user:user@db/policeCalls')
+        con = engine.connect()
+        for item in pd.read_csv('data/police-department-incidents.csv', chunksize=1000):
+            item.to_sql('police_incidents', con, if_exists='append')
+    except Exception as e:
+        db = False
+    else:
+        db = True
